286b9c96629ca938c61ace21908831ec8643b021
lokesh reddy <97579926+Sabbellalokesh@users.noreply.github.com>
2022-07-02 10:12:52 +0530

    n=int(input()) l=list(map(int,input().split())) for i in range(0,n,2):     for j in range(l[i+1]):         print(l[i],end=' ')

--- Generate_array_II.py
+++ Generate_array_II.py
@@ -1,9 +1,5 @@
-
 n=int(input())
-arr=list(map(int,input().split()))
-newarr=[]
-for i in range(n):
-    if i%2==0:
-        for j in range(arr[i+1]):
-            newarr.append(arr[i])
-print(*newarr)
+l=list(map(int,input().split()))
+for i in range(0,n,2):
+    for j in range(l[i+1]):
+        print(l[i],end=' ')
