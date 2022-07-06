a9a66d14dfab9fd9835b21074483da1c05f99c23
lokesh reddy <97579926+Sabbellalokesh@users.noreply.github.com>
2022-07-06 20:27:11 +0530

    n=input() n=n.lower() a=[] for i in range(len(n)):     if n[i]!=' ':         a.append(n[i]) a=list(set(a)) a.sort() for i in range(len(a)):     print(a[i],end='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -2,12 +2,9 @@ n=input()
 n=n.lower()
 a=[]
 for i in range(len(n)):
-    c=0
-    for j in range(len(n)):
-        if n[i]==n[j] and n[i]!=' ':
-            c+=1
-    if c==1:
+    if n[i]!=' ':
         a.append(n[i])
+a=list(set(a))
 a.sort()
 for i in range(len(a)):
     print(a[i],end='')
