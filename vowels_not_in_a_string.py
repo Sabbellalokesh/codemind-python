73ad43fec7816c15d369419abfa90d12196dfa61
lokesh reddy <97579926+Sabbellalokesh@users.noreply.github.com>
2022-07-06 19:01:18 +0530

    s='aeiou' n=input() a=[] for i in s:     c=0     for j in n:         if i==j:             c+=1     if c==0:         a.append(i) if len(a)>0:     print(*a) else:     print('0')

--- vowels_not_in_a_string.py
+++ vowels_not_in_a_string.py
@@ -1,25 +1,14 @@
-s=input()
-a,e,i,o,u=0,0,0,0,0
-for char in s:
-    if char=='a':
-        a+=1
-    if char=='e':
-        e+=1
-    if char=='i':
-        i+=1
-    if char=='o':
-        o+=1
-    if char=='u':
-        u+=1
-if a==0:
-    print('a',end=' ')
-if e==0:
-    print('e',end=' ')
-if i==0:
-    print('i',end=' ')
-if o==0:
-    print('o',end=' ')
-if u==0:
-    print('u',end=' ')
-if a!=0 and e!=0 and i!=0 and o!=0 and u!=0:
-    print(0)
+s='aeiou'
+n=input()
+a=[]
+for i in s:
+    c=0
+    for j in n:
+        if i==j:
+            c+=1
+    if c==0:
+        a.append(i)
+if len(a)>0:
+    print(*a)
+else:
+    print('0')
