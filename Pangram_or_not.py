d8ce310be94f1d8137998643475cdc4f93dcf21d
lokesh reddy <97579926+Sabbellalokesh@users.noreply.github.com>
2022-07-07 22:22:06 +0530

    l=input() l=l.lower() a=[] for i in range(len(l)):     if l[i]!=' ':         a.append(l[i]) a=set(a) if len(a)==26:     print(True) else:     print(False)

--- Pangram_or_not.py
+++ Pangram_or_not.py
@@ -1,11 +1,11 @@
-n=input()
-n=n.lower()
+l=input()
+l=l.lower()
 a=[]
-for i in n:
-    if i.isalpha():
-        a.append(i)
+for i in range(len(l)):
+    if l[i]!=' ':
+        a.append(l[i])
 a=set(a)
 if len(a)==26:
-    print("True")
+    print(True)
 else:
-    print("False")
+    print(False)
