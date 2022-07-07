103ae256ab1eab3ee13ec186535bfe0863375ab3
lokesh reddy <97579926+Sabbellalokesh@users.noreply.github.com>
2022-07-07 13:31:07 +0530

    l=list(map(str,input().split())) s="aeiouAEIOU" c=0 for i in l:     if i[0] in s:         c+=1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,7 +1,7 @@
 l=list(map(str,input().split()))
+s="aeiouAEIOU"
 c=0
 for i in l:
-    if i[0] in 'aeiouAEIOU':
-        if i[len(i)-1] not in 'aeiouAEIOU':
-            c+=1
+    if i[0] in s:
+        c+=1
 print(c)
