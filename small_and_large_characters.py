ff00b525a2957a4ec246d05c3df67f3ce3a99ee6
lokesh reddy <97579926+Sabbellalokesh@users.noreply.github.com>
2022-07-07 14:09:00 +0530

    l=list(map(str,input().split())) a=[] for i in l:     a.append(min(i))     a.append(max(i)) print(*a)

--- small_and_large_characters.py
+++ small_and_large_characters.py
@@ -1,7 +1,6 @@
 l=list(map(str,input().split()))
-ll=len(l)
 a=[]
-for i in range(ll):
-    a.append(min(l[i]))
-    a.append(max(l[i]))
+for i in l:
+    a.append(min(i))
+    a.append(max(i))
 print(*a)
