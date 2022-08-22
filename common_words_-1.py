c548da4467fcf0eb0e11ef7bed158162b12ddf7a
lokesh reddy <97579926+Sabbellalokesh@users.noreply.github.com>
2022-08-22 12:05:52 +0530

    l=list(map(str,input().split())) ll=[] ll1=[] s=[] for i in l:     ll.append(i.lower()) l1=list(map(str,input().split())) for i in l1:     ll1.append(i.lower()) for i in ll:     if i in ll1:         s.append(i) print(len(s))

--- common_words_-1.py
+++ common_words_-1.py
@@ -1,15 +1,13 @@
 l=list(map(str,input().split()))
-ll=list(map(str,input().split()))
-s=0
-for i in range(len(l)):
-    l[i]=l[i].lower()
-for i in range(len(ll)):
-    ll[i]=ll[i].lower()
+ll=[]
+ll1=[]
+s=[]
 for i in l:
-    c=0
-    for j in ll:
-        if i==j:
-            c+=1
-    if c>0:
-        s+=1
-print(s)   
+    ll.append(i.lower())
+l1=list(map(str,input().split()))
+for i in l1:
+    ll1.append(i.lower())
+for i in ll:
+    if i in ll1:
+        s.append(i)
+print(len(s))
