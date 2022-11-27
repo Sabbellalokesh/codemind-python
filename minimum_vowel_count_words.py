9cb7d7255bb453602036e2f7a1bd2bf81ff9ac5a
lokesh reddy <97579926+Sabbellalokesh@users.noreply.github.com>
2022-11-27 11:31:54 +0530

    l=list(map(str,input().split())) d=[] for i in l:     count=0     for j in i:         if j in "aeiouAEIOU":             count=count+1     d.append(count) dup=[] for i in d:     if i == min(d):          dup.append(i) print(len(dup))

--- minimum_vowel_count_words.py
+++ minimum_vowel_count_words.py
@@ -1,20 +1,13 @@
 l=list(map(str,input().split()))
-arr=[]
-if len(l)==1:
-    c=0
-    for char in l[0]:
-        if char in 'aeiouAEIOU':
-            c=1
-    print(c)
-else:
-    for ch in l:
-        c=0
-        for char in ch:
-            if char in 'aeiouAEIOU':
-                c+=1
-        arr.append(c)
-    c=0
-    for i in range(len(arr)):
-        if arr[i]==min(arr):
-            c+=1
-    print(c)
+d=[]
+for i in l:
+    count=0
+    for j in i:
+        if j in "aeiouAEIOU":
+            count=count+1
+    d.append(count)
+dup=[]
+for i in d:
+    if i == min(d):
+         dup.append(i)
+print(len(dup)) 
